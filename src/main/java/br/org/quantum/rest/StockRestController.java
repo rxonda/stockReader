eea7502f205eabf5a5d9ab953b35182295625308
xonda <rxonda@gmail.com>
2015-03-16 03:26:48 -0300

    frontend

--- src/main/java/br/org/quantum/rest/StockRestController.java
+++ src/main/java/br/org/quantum/rest/StockRestController.java
@@ -1,10 +1,10 @@
 package br.org.quantum.rest;
 
 import br.org.quantum.domain.Movimento;
+import br.org.quantum.domain.VolumeMedio;
 import br.org.quantum.services.StockReaderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,14 +13,38 @@ import java.util.Collection;
 /**
  * Created by xonda on 13/03/2015.
  */
-@RestController("/stocks")
+@RestController
 public class StockRestController {
     @Autowired
     private StockReaderService stockReaderService;
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping("/all")
     public @ResponseBody Collection<Movimento> list() {
         return stockReaderService.list();
     }
 
+    @RequestMapping("/fechamentoMaximo")
+    public @ResponseBody Collection<Movimento> listFechamentoMaximo() {
+        return stockReaderService.fechamentosMaximo();
+    }
+
+    @RequestMapping("/fechamentoMinimo")
+    public @ResponseBody Collection<Movimento> listFechamentoMinimo() {
+        return stockReaderService.fechamentosMinimo();
+    }
+
+    @RequestMapping("/retornoMaximo")
+    public @ResponseBody Collection<Movimento> listRetornoMaximo() {
+        return stockReaderService.retornosMaximo();
+    }
+
+    @RequestMapping("/retornoMinimo")
+    public @ResponseBody Collection<Movimento> listRetornoMinimo() {
+        return stockReaderService.retornosMinimo();
+    }
+
+    @RequestMapping("/volumeMedio")
+    public @ResponseBody Collection<VolumeMedio> listVolumeMedio() {
+        return stockReaderService.volumesMedio();
+    }
 }
--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -3,12 +3,58 @@
 */
 var stockReaderApp=angular.module('stockReaderApp', []);
 
-stockReaderApp.controller('stockReaderController', function($scope, $http){
-//            $scope.movimentacao = [
-//                {id: 'BRAD', date:'2014-01-01', close: '10.00', volume: '200'}
-//            ];
-    $http.get('/stocks').success(function(data){
-        $scope.movimentacao = data;
-    });
-});
+stockReaderApp.controller('stockReaderController',['$scope', '$http', function(scope, http){
+    scope.mostraVolume=false;
+    scope.mostraMovimento=false;
+
+    scope.showAll = function() {
+        scope.mostraMovimento = true;
+        scope.mostraVolume=false;
+        http.get('/all').success(function(data){
+            scope.movimentacao = data;
+        });
+    };
+
+    scope.fechamentoMaximo = function() {
+        scope.mostraMovimento = true;
+        scope.mostraVolume=false;
+        http.get('/fechamentoMaximo').success(function(data){
+            scope.movimentacao = data;
+        });
+    };
+
+    scope.fechamentoMinimo = function() {
+        scope.mostraMovimento = true;
+        scope.mostraVolume=false;
+        http.get('/fechamentoMinimo').success(function(data){
+            scope.movimentacao = data;
+        });
+    };
+
+    scope.retornoMaximo = function() {
+        scope.mostraMovimento = true;
+        scope.mostraVolume=false;
+        http.get('/retornoMaximo').success(function(data){
+            scope.movimentacao = data;
+        });
+    };
+
+    scope.retornoMinimo = function() {
+        scope.mostraMovimento = true;
+        scope.mostraVolume=false;
+        http.get('/retornoMinimo').success(function(data){
+            scope.movimentacao = data;
+        });
+    };
+
+    scope.volumeMedio = function() {
+        scope.mostraMovimento = false;
+        scope.mostraVolume=true;
+        http.get('/volumeMedio').success(function(data){
+            scope.volumes = data;
+        });
+    };
+
+//            scope.showAll();
+}]);
 
