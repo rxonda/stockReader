fc2977d7a7d6863f86d78ed82f102a347846629d
xonda <rxonda@gmail.com>
2015-03-16 23:34:42 -0300

    separando os templates em arquivos e usando ngRoute

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,60 +1,71 @@
 /**
   xonda
 */
-var stockReaderApp=angular.module('stockReaderApp', []);
+var stockReaderApp=angular.module('stockReaderApp', ['ngRoute']);
+
+stockReaderApp.config(function($routeProvider){
+   $routeProvider.
+       when('/', {
+           templateUrl: './table-movimento.html',
+           controller: 'stockReaderController'
+       }).
+       when('/fechamentoMaximo', {
+           templateUrl: './table-movimento.html',
+           controller: 'fechamentoMaximoController'
+       }).
+       when('/fechamentoMinimo', {
+           templateUrl: './table-movimento.html',
+           controller: 'fechamentoMinimoController'
+       }).
+       when('/retornoMaximo', {
+           templateUrl: './table-movimento.html',
+           controller: 'retornoMaximoController'
+       }).
+       when('/retornoMinimo', {
+           templateUrl: './table-movimento.html',
+           controller: 'retornoMinimoController'
+       }).
+       when('/volumeMedio', {
+           templateUrl: './table-volume.html',
+           controller: 'volumeMedioController'
+       }).
+       otherwise({
+           redirectTo: '/'
+       });
+});
 
 stockReaderApp.controller('stockReaderController',['$scope', '$http', function(scope, http){
-    scope.mostraVolume=false;
-    scope.mostraMovimento=false;
-
-    scope.showAll = function() {
-        scope.mostraMovimento = true;
-        scope.mostraVolume=false;
-        http.get('/all').success(function(data){
-            scope.movimentacao = data;
-        });
-    };
-
-    scope.fechamentoMaximo = function() {
-        scope.mostraMovimento = true;
-        scope.mostraVolume=false;
-        http.get('/fechamentoMaximo').success(function(data){
-            scope.movimentacao = data;
-        });
-    };
-
-    scope.fechamentoMinimo = function() {
-        scope.mostraMovimento = true;
-        scope.mostraVolume=false;
-        http.get('/fechamentoMinimo').success(function(data){
-            scope.movimentacao = data;
-        });
-    };
-
-    scope.retornoMaximo = function() {
-        scope.mostraMovimento = true;
-        scope.mostraVolume=false;
-        http.get('/retornoMaximo').success(function(data){
-            scope.movimentacao = data;
-        });
-    };
-
-    scope.retornoMinimo = function() {
-        scope.mostraMovimento = true;
-        scope.mostraVolume=false;
-        http.get('/retornoMinimo').success(function(data){
-            scope.movimentacao = data;
-        });
-    };
-
-    scope.volumeMedio = function() {
-        scope.mostraMovimento = false;
-        scope.mostraVolume=true;
-        http.get('/volumeMedio').success(function(data){
-            scope.volumes = data;
-        });
-    };
-
-//            scope.showAll();
+    http.get('/all').success(function(data){
+        scope.movimentacao = data;
+    });
 }]);
 
+stockReaderApp.controller('fechamentoMaximoController',['$scope', '$http', function(scope, http) {
+    http.get('/fechamentoMaximo').success(function(data){
+        scope.movimentacao = data;
+    });
+}]);
+
+stockReaderApp.controller('fechamentoMinimoController',['$scope', '$http', function(scope, http) {
+    http.get('/fechamentoMinimo').success(function(data){
+        scope.movimentacao = data;
+    });
+}]);
+
+stockReaderApp.controller('retornoMaximoController',['$scope', '$http', function(scope, http) {
+    http.get('/retornoMaximo').success(function(data){
+        scope.movimentacao = data;
+    });
+}]);
+
+stockReaderApp.controller('retornoMinimoController',['$scope', '$http', function(scope, http) {
+    http.get('/retornoMinimo').success(function(data){
+        scope.movimentacao = data;
+    });
+}]);
+
+stockReaderApp.controller('volumeMedioController',['$scope', '$http', function(scope, http) {
+    http.get('/volumeMedio').success(function(data){
+        scope.volumes = data;
+    });
+}]);
